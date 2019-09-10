6b0b750529ded8a74c868cbe3f0ff92b0308e643
julianoduarte1983 <40240663+julianoduarte1983@users.noreply.github.com>
2019-09-10 14:30:24 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,34 +9,31 @@ class Recipe(scrapy.Item):
 
 class MyFirstSpyder(scrapy.Spider):
     name = "my_first_spider"
-    allowed_domains = 'https://www.tudogostoso.com.br'
+    allowed_domains = 'tudogostoso.com.br'
     start_urls = [
         'https://www.tudogostoso.com.br/categorias/1004-carnes'
     ]
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0'
-    }
+
     custom_settings = {
-        'CONCURRENT_REQUESTS': '3',
-        'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0'
+        'CONCURRENT_REQUESTS': '1',
+        'LOG_LEVEL': 'ERROR',
+        'DOWNLOADER_MIDDLEWARES' : {
+                'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+                'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
+            }
     }
 
     def parse(self, response):
-        #import ipdb #; ipdb.set_trace()
-        # pegar os links de todas as receitas da pagina atual
-        #base_url = 'https://www.tudogostoso.com.br'
         for recipe in response.css('div.card a::attr(href)').getall():
-            #ipdb.set_trace()
             yield scrapy.Request(
                 url=response.urljoin(recipe),
-                headers=self.headers,
-                callback=self.parse_recipes,
-                errback=self.parse_error
+                callback=self.parse_recipes
             )
+            #self.logger.error(repr(response))
 
     def parse_recipes(self, response):
-        #import ipdb; ipdb.set_trace()
         print("PARSE_PARSE")
+        '''
         _author = response.xpath('/html/body/div[8]/div[1]/div/div[3]/div[1]/div/div[1]/div[2]/div[2]/div/div/div[5]/div').extract()
         _name = response.xpath('//*[@id="item-id-1251"]').extract()
         _ingr = response.xpath('/html/body/div[7]/div[1]/div/div[3]/div[4]').extract()
@@ -47,7 +44,7 @@ class MyFirstSpyder(scrapy.Spider):
             'ingr': _ingr,
             'prep': _prep
         })
+        '''
 
     def parse_error(self, failure):
         self.logger.error(repr(failure))
-
